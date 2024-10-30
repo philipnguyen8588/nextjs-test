97755f3b7fbe1689a8b37e163b46403bbfd95599
LipNguyen <philip.nguyen8588@gmail.com>
2024-10-30 20:45:25 +0700

    Create pipeline stories book

---
+++ stories/Pipeline.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Pipeline } from './Pipeline';
+
+const meta: Meta<typeof Pipeline> = {
+  title: 'Example/Pipeline',
+  component: Pipeline,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Pipeline>;
+
+export const Light: Story = {
+  args: {
+    name: 'Pipeline Name',
+    title: 'Your pipeline is being configured',
+    subTitle: 'The build process can take up to two hours.\nWe will send you an email when setup is complete',
+    status: 'Building',
+    totalTime: 10000,
+    elapsedTime: 1000,
+    theme: 'light',
+  },
+};
+
+export const Dark: Story = {
+  args: {
+    name: 'Pipeline Name',
+    title: 'Your pipeline is being configured',
+    subTitle: 'The build process can take up to two hours.\nWe will send you an email when setup is complete',
+    status: 'Building',
+    totalTime: 10000,
+    elapsedTime: 1000,
+    theme: 'dark',
+  },
+};
